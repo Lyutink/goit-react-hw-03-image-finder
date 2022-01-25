04163cd2988ab8745f24747cf28eca66306f8bc9
Lyutink <krivenko.lyuda@gmail.com>
2022-01-25 22:35:47 +0300

    _

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Searchbar from "./Components/Searchbar/Searchbar";
+// 'edle' стоит на месте, простой
+// 'pending' ожидается выполнения
+// 'resolved' выполнилось с результатом
+// 'rejected' отклонено
+
+class App extends Component {
+  state = {
+    requestFromUser: "",
+    status: "edle",
+    on: 1,
+  };
+
+  handlerSubmit({ requestFromUser }) {
+    console.log("handlerSubmit:", requestFromUser);
+    //this.setState({ requestFromUser: "hhhh" });
+  }
+
+  render() {
+    //const (status, ) = this.state;
+    if (this.state.status === "edle") {
+      console.log("ffff");
+    }
+
+    return <Searchbar onSubmit={this.handlerSubmit} />;
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -1,5 +1,7 @@
-// Компонент принимает один проп onSubmit - функцию для передачи значения инпута при сабмите формы. Создает DOM-элемент следующей структуры.
-
+// Компонент принимает один проп onSubmit - функцию для
+// передачи значения инпута при сабмите формы.
+//Создает DOM - элемент следующей структуры.
+import React, { Component } from "react";
 // <header class="searchbar">
 //   <form class="form">
 //     <button type="submit" class="button">
@@ -14,4 +16,49 @@
 //       placeholder="Search images and photos"
 //     />
 //   </form>
-// </header>
+// </header>{
+class Searchbar extends Component {
+  state = {
+    requestFromUser: "",
+  };
+
+  handleChange = (event) => {
+    // this.setState({
+    //     [event.currentTarget.name]: event.currentTarget.value,
+    // });
+    console.log(event.currentTarget.value);
+    this.setState({ requestFromUser: event.currentTarget.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state);
+    // this.reset();
+  };
+
+  // reset = () => {
+  //       this.setState({ requestFromUsere: ''});
+  // }
+
+  render() {
+    return (
+      <header>
+        <form onSubmit={this.handleSubmit}>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+
+          <input
+            onChange={this.handleChange}
+            type="text"
+            //autocomplete="off"
+            //autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+export default Searchbar;
